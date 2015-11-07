4b8c8e4aad6523df64d6a43ef2c97a9eac07ee9e
Di Ruan <dr2763@columbia.edu>
2015-11-07 18:11:46 -0500

    add links to unjoined and joined session

--- public/javascripts/user_page.js
+++ public/javascripts/user_page.js
@@ -1,7 +1,12 @@
-function joinSession() {
+function joinSession(sessionId, userId) {
     console.log("join session");
+    console.log(window.location.hostname);
+    var url = window.location.hostname + "/joinSession/" + sessionId + "/" + userId;
+    window.location.href = url;
 }
 
-function showSession() {
+function showSession(sessionId, userId) {
     console.log("show session");
+    var url = window.location.hostname + "/session/" + sessionId + "/" + userId;
+    window.location.href = url;
 }
