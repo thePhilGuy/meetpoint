59cc499fc961068ef24af563019150ca044155fe
mbl2109 <mbl2109@columbia.edu>
2015-11-11 18:33:45 -0500

    changed @f, added console print in javascript

--- public/javascripts/session_page.js
+++ public/javascripts/session_page.js
@@ -1,4 +1,5 @@
 function inviteUser(sessionId, userName) {
+    console.log("javascript inviteUser called");
     var url = window.location.origin + "/inviteUser/" + sessionId + "/" + userName;
     $.ajax({
         url: url,
