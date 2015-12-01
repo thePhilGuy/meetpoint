c08c28161eefaf7ec37862f044e7118bca53f288
Di Ruan <dr2763@columbia.edu>
2015-12-01 09:21:57 -0500

    start adding tests

--- test/IntegrationTest.java
+++ test/IntegrationTest.java
@@ -19,8 +19,8 @@ public class IntegrationTest {
     public void test() {
         running(testServer(3333, fakeApplication(inMemoryDatabase())), HTMLUNIT, new Callback<TestBrowser>() {
             public void invoke(TestBrowser browser) {
-                browser.goTo("http://localhost:3333");
-                assertThat(browser.pageSource()).contains("Your new application is ready.");
+                browser.goTo("http://www.meetpoint.com:3333");
+                assertThat(browser.pageSource()).contains("Welcome to MeetPoint");
             }
         });
     }
